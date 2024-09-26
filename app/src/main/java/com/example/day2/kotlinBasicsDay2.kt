a1ab9fa84c29ca41d273c99b6d063131b481fdaa
rokshelby <rokshelby@gmail.com>
2024-09-26 06:33:29 -0500

    using data classes.

--- app/src/main/java/com/example/day2/kotlinBasicsDay2.kt
+++ app/src/main/java/com/example/day2/kotlinBasicsDay2.kt
@@ -1,38 +1,27 @@
 package com.example.day2
 
 import androidx.core.text.isDigitsOnly
-
+data class CoffeeDetails(
+    val sugarCount : Int,
+    val name : String,
+    val size : String,
+    val creamAmount : Int
+)
 fun main(){
-
-    println("Enter the customer name")
-    var name = readln()
-
-
-    println("Enter the number of sugars")
-    var numOfSugars = readln().toIntOrNull()
-    if(numOfSugars == null){
-        numOfSugars = 0
-    }
-
-    if(name == ""){
-        name = "Customer"
-    }
+    val coffeeForRakeem = CoffeeDetails(0, "rakeem", "tall", 0)
+    makeCoffee(coffeeForRakeem)
 
 
-    makeCoffee(numOfSugars,name)
+
+
 }
-fun makeCoffee(sugarCount : Int, name : String){
-//    if(sugarCount == 0){
-//        println("Coffee with no sugar for $name")
-//    }
-//    else if(sugarCount == 1){
-//        println("Coffee with $sugarCount spoon of sugar for $name")
-//    }else{
-//        println("Coffee with $sugarCount spoons of sugar for $name")
-//    }
-    when(sugarCount){
-        0->println("Coffee with no sugar for $name")
-        1->println("Coffee with $sugarCount spoon of sugar for $name")
-        else->println("Coffee with $sugarCount spoons of sugar for $name")
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    when(coffeeDetails.sugarCount){
+        0->println("Coffee with no sugar for ${coffeeDetails.name} "
+        + "and cream amount :${coffeeDetails.creamAmount}")
+        1->println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name} "
+        + "and cream amount :${coffeeDetails.creamAmount}")
+        else->println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name} "
+                + "and cream amount : ${coffeeDetails.creamAmount}")
     }
 }
